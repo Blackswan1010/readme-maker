c18f8858e0fb53939277fef32751e7d8a4dc3be9
Anthony Nguyen <anthonynguye56@gmail.com>
2023-10-20 00:55:14 -0700

    add code to function renderLicenseLink, add additional prompts to questions

--- index.js
+++ index.js
@@ -5,45 +5,63 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-        {
-            message: 'What is the title of your project?',
-            type: 'input',
-            name: 'title',
-        },
-        {
-            message: 'What is the name of the Github repository?',
-            type: 'input',
-            name: 'githubName',
-        },
-        {
-            message: 'Enter a description for your project.',
-            type: 'input',
-            name: 'description',
-        },
-        {
-            message: 'Enter a description for your project.',
-            type: 'list',
-            name: 'description',
-            choices: ['mit', 'agpl', 'apache', 'unlicense', 'bsd-2-clause'],
-        }
-    ];
-    
+    {
+        message: 'What is your name?',
+        type: 'input',
+        name: 'name',
+    },
+    {
+        message: 'What is the title of your project?',
+        type: 'input',
+        name: 'title',
+    },
+    {
+        message: 'Enter a description for your project.',
+        type: 'input',
+        name: 'description',
+    },
+    {
+        message: 'Choose a license for your project.',
+        type: 'list',
+        name: 'license',
+        choices: ['MIT', 'Apache 2.0', 'General Public v3', 'BSD 2-Clause', 'BSD 3-Clause', 'Boost Software', 'Creative Commons Zero', 'Eclipse Public', 'Unlicense', 'Mozzila Public']
+    },
+    {
+        message: 'Enter a description for your project.',
+        type: 'input',
+        name: 'description',
+    },
+    {
+        message: 'Enter a description for your project.',
+        type: 'input',
+        name: 'description',
+    },
+    {
+        message: 'What is the name of the Github repository?',
+        type: 'input',
+        name: 'githubName',
+    },
+];
+
+
+
 //* add type, name, and message for each question
 //* add questions:  added readme sections (description, usage, license, etc)
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {
     //* use FS module to write data and fileName to readme file
-    fs.writeFile(fileName, data);
-
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
 }
 
 // TODO: Create a function to initialize app
-const init = () => { 
+const init = () => {
     inquirer
         .prompt(questions)
         .then((response) => {
-            writeToFile('README.md',generateMarkdown(response));
+            writeToFile('README.md', generateMarkdown(response));
         })
 }
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,67 +1,119 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 //*must have a couple license options
+const axios = require("axios");
 
-function renderLicenseBadge(license) {
+const renderLicenseBadge = (license) => {
+  switch (license) {
+    case 'MIT':
+      return;
 
-  return;
+    case 'Apache 2.0':
+      return;
+
+    case 'General Public v3':
+      return;
+
+    case 'BSD 2-Clause':
+      return;
+
+    case 'BSD 3-Clause':
+      return;
+    case 'Boost Software':
+      return;
+
+    case 'Creative Commons Zero':
+      return;
+
+    case 'Eclipse Public':
+      return;
+
+    case 'Unlicense':
+      return;
+
+    case 'Mozzila Public':
+      return;
+
+    default: return '';
+  }
+
 }
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 
-function renderLicenseLink(license) {
-//* return a string that has the link to selected license
-return;
+const renderLicenseLink = (license) => {
+  //* return a string that has the link to selected license
+
+  if (license) {
+    return axios.get("https://api.github.com/licenses/" + license);;
+  } else {
+    return '';
+  }
+
 }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 
-function renderLicenseSection(license) {
+const renderLicenseSection = (license) => {
   //* create a conditional to check if license exists
   //* call renderlicenseBadge and renderlicenseLink
   //* use template literal to format license section and call functions
   //* return template literal
+  if (renderLicenseBadge(license) && renderLicenseLink(license)) {
+    return renderLicenseBadge(license);
+  } else {
+    return '';
+  }
 
-  return;
 }
 // TODO: Create a function to generate markdown for README
 
-function generateMarkdown(data) {
+const generateMarkdown = (data) => {
   //* create layout of what you markdown readme with template literals
   //* hint - call renderlicense functions inside this function
   return `# ${data.title} \n
 
   ## Description \n
-  ${data.description} \n\n
+  ${data.description} \n
 
   ## Table of Contents \n
-  ${data.contents} \n\n
+  ${data.contents} \n
 
   ## Installation \n
-  ${data.installation} \n\n
+  ${data.installation} \n
 
   ## Usage \n
-  ${data.usage} \n\n
+  ${data.usage} \n
+
+  ## Author Info \n
+  #### ${data.name}\n
+  ``md\n
+  * [Github](https://github.com/${data.githubName}) \n
+  ``\n
 
   ## Credits \n
-  ${data.credits} \n\n
+  ${data.credits} \n
 
   ## License \n
-  ${data.license} \n\n
+  ${data.license} \n
 
   ## Badges \n
-  ${data.badges} \n\n
+  ${data.badges} \n
 
   ## Features \n
-  ${data.features} \n\n
+  ${data.features} \n
 
   ## Contributing \n
-  ${data.contribution} \n\n
+  ${data.contribution} \n
 
   ## Tests \n
-  ${data.tests} \n\n
+  ${data.tests} \n
 
-`;
+`
+  renderLicenseSection();
+
+  ;
+
 }
 
 module.exports = generateMarkdown;
